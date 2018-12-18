6e3c3327915639fabeffc81f39b55f2a8328c041
Denys Khlivnyy <testoftiramisu@gmail.com>
2018-12-18 15:49:51 +0100

    Example of first test.

--- src/test/java/romannumerals/RomanToNumeralsConverterTest.java
+++ src/test/java/romannumerals/RomanToNumeralsConverterTest.java
@@ -1,5 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package romannumerals;
+
+import org.junit.jupiter.api.Test;
 
 class RomanToNumeralsConverterTest {
+
+  RomanToNumeralsConverter converter;
+
+  @Test
+  public void convertICharacterToOne() {
+    assertThat(converter.convert("I")).isEqualTo(1);
+  }
 
 }
